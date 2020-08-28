461cdccaa709c9edbb43df6d427c355e8f653332
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-28 14:01:20 +0000

    Automatically backed up by Learn

--- spec/my_first_hash_spec.rb
+++ spec/my_first_hash_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 require_relative '../my_first_hash.rb'
 def my_first_hash
-  your_hash= {"pineapple" => "Best Fruit", "peanuts" => "I'm not allergic", 
+  your_hash= {"pineapple" => "Best Fruit", "peanuts" => "I'm not allergic", "Mickey Mouse" => "Favourite character so far"}
 
 context "Challenge I: Instantiating Hashes" do
   describe "#my_hash" do
